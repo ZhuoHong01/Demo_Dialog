d6de0ebae6d8759e404fe0f4789ddc1c8da92f3e
lee zhuo hong <21018193@myrp.edu.sg>
2022-08-03 09:42:01 +0800

    zh - commit

--- app/src/main/java/sg/edu/rp/c346/id21018193/demodialog/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/demodialog/MainActivity.java
@@ -1,14 +1,38 @@
 package sg.edu.rp.c346.id21018193.demodialog;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnDemo1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnDemo1 = findViewById(R.id.buttonDemo1);
+
+        btnDemo1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Create the Dialog Builder
+                AlertDialog.Builder myBuilder = new AlertDialog.Builder(MainActivity.this);
+
+                //Set the Dialog details
+                myBuilder.setTitle("Demo 1 - Simple Dialog");
+                myBuilder.setMessage("I can develop Android App");
+                myBuilder.setCancelable(false);
+                myBuilder.setPositiveButton("Close", null);
+
+                AlertDialog myDialog = myBuilder.create();
+                myDialog.show();
+            }
+        });
     }
 }
